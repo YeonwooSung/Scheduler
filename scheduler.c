f62eb75dd30ad9aa3d5e509ff788addcb6ad7631
YeonwooSung <neos960518@gmail.com>
2018-11-08 20:13:56 +0900

    add some comments to the makeQueue()

--- scheduler.c
+++ scheduler.c
@@ -5,13 +5,19 @@ typedef struct ready_queue {
     struct ready_queue *next;
 } ReadyQueue;
 
+/**
+ * The aim of this function is to allocate the memory to make the ready queue.
+ *
+ * @param (p_list) the linked list of process control blocks
+ * @return The pointer that points to the head node of the generated ready queue.
+ */
 ReadyQueue *makeQueue(PCB *p_list) {
     ReadyQueue *queue = (ReadyQueue *) malloc(sizeof(ReadyQueue));
 
     queue->process = p_list;
     p_list = p_list->next;
 
-    if (p_list) {
+    if (p_list) { // check if there are more process control blocks in the linked list of pcb.
         queue->next = makeQueue(p_list);
     } else {
         queue->next = NULL;
